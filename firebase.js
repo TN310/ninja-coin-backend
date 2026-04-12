ee96b47c44cae06850ea8917d877d6bfeb2a21ef
עומר <OMERTN310@GMAIL.COM>
2026-04-12 16:20:40 +0300

    Support Render deployment with inline Firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
+let credential;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  credential = admin.credential.cert(serviceAccount);
+} else {
+  credential = admin.credential.applicationDefault();
+}
+
 admin.initializeApp({
+  credential,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 const db = admin.database();
-
 module.exports = db;
